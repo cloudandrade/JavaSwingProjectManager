af7503565a2421469a43331746996d13cee3c72a
Jan Cloud Andrade <cloud.andrade16@gmail.com>
2024-05-25 16:59:43 -0300

    fix database ods inserts

--- docs/mysql/dev.sql
+++ docs/mysql/dev.sql
@@ -32,20 +32,20 @@ CREATE TABLE IF NOT EXISTS `Project` (
 INSERT INTO user (username, name, email, phone, password) VALUES ("adm", "admin", "admin@pm.com", "000000000", "$2a$10$5rREqwYq0bX0o4jG/3uP3ejx1Y9bB.ftmsmsoOHhLrRiDtFkoND8q"); 
 -- encrypted password Admin@24k
 
-INSERT INTO ods (title) values ("ERRADICAÇÃO DA POBREZA");
-INSERT INTO ods (title) values ("FOME ZERO E AGRICULTURA SUSTENTÁVEL");
-INSERT INTO ods (title) values ("SAÚDE E BEM-ESTAR");
-INSERT INTO ods (title) values ("EDUCAÇÃO DE QUALIDADE");
-INSERT INTO ods (title) values ("IGUALDADE DE GÊNERO");
-INSERT INTO ods (title) values ("ÁGUA POTÁVEL");
-INSERT INTO ods (title) values ("ENERGIA LIMPA E ACESSÍVEL");
-INSERT INTO ods (title) values ("TRABALHO DECENTE E CRESCIMENTO ECONÔMICO");
-INSERT INTO ods (title) values ("INDÚSTRIA, INOVAÇÃO E INFRAESTRUTURA");
-INSERT INTO ods (title) values ("REDUÇÃO DAS DESIGUALDADES");
-INSERT INTO ods (title) values ("CIDADES E COMUNIDADES SUSTENTÁVEIS");
-INSERT INTO ods (title) values ("CONSUMO E PRODUÇÃO RESPONSÁVEIS");
-INSERT INTO ods (title) values ("AÇÃO CONTRA A MUDANÇA GLOBAL DO CLIMA");
-INSERT INTO ods (title) values ("VIDA NA ÁGUA");
-INSERT INTO ods (title) values ("VIDA TERRESTRE");
-INSERT INTO ods (title) values ("PAZ, JUSTIÇA E INSTITUIÇÕES EFICAZES");
-INSERT INTO ods (title) values ("PARCERIAS E MEIOS DE IMPLEMENTAÇÃO");
+INSERT INTO ods (id, title) values (1,"ERRADICAÇÃO DA POBREZA");
+INSERT INTO ods (id,title) values (2,"FOME ZERO E AGRICULTURA SUSTENTÁVEL");
+INSERT INTO ods (id,title) values (3,"SAÚDE E BEM-ESTAR");
+INSERT INTO ods (id,title) values (4,"EDUCAÇÃO DE QUALIDADE");
+INSERT INTO ods (id,title) values (5,"IGUALDADE DE GÊNERO");
+INSERT INTO ods (id,title) values (6,"ÁGUA POTÁVEL");
+INSERT INTO ods (id,title) values (7,"ENERGIA LIMPA E ACESSÍVEL");
+INSERT INTO ods (id,title) values (8,"TRABALHO DECENTE E CRESCIMENTO ECONÔMICO");
+INSERT INTO ods (id,title) values (9,"INDÚSTRIA, INOVAÇÃO E INFRAESTRUTURA");
+INSERT INTO ods (id,title) values (10,"REDUÇÃO DAS DESIGUALDADES");
+INSERT INTO ods (id,title) values (11,"CIDADES E COMUNIDADES SUSTENTÁVEIS");
+INSERT INTO ods (id,title) values (12,"CONSUMO E PRODUÇÃO RESPONSÁVEIS");
+INSERT INTO ods (id,title) values (13,"AÇÃO CONTRA A MUDANÇA GLOBAL DO CLIMA");
+INSERT INTO ods (id,title) values (14,"VIDA NA ÁGUA");
+INSERT INTO ods (id,title) values (15,"VIDA TERRESTRE");
+INSERT INTO ods (id,title) values (16,"PAZ, JUSTIÇA E INSTITUIÇÕES EFICAZES");
+INSERT INTO ods (id,title) values (17,"PARCERIAS E MEIOS DE IMPLEMENTAÇÃO");
